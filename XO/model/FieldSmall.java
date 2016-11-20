499d64d47f501a87186ca322ff4c9be553d63241
apple <apple@Air-apple-2.Dlink>
2016-11-20 20:26:36 +0300

    create FieldSmall model #9

---
+++ XO/model/FieldSmall.java
@@ -0,0 +1,19 @@
+package xo.model;
+
+public class FieldSmall {
+
+	private final Figure[][] figures = new Figure[3][3];
+
+	public int getSize() {
+		return figures.length;
+    	}
+
+   	public Figure getFigure(final Point point) {
+		return figures[point.getX()][point.getY()];
+    	}
+
+    	public void setFigure(final Point point, final Figure figure) {
+		figures[point.getX()][point.getY()] = figure;
+    	}
+
+}
