f41a95fe8ae91b4ab7f87f4796ddff4b451dc3fd
evgenymatveev <evgeni2007@yandex.ru>
2016-12-19 01:57:21 +0300

    ConsoleView checkstile

--- src/main/java/ru/ematveev/view/ConsoleView.java
+++ src/main/java/ru/ematveev/view/ConsoleView.java
@@ -11,32 +11,32 @@ public class ConsoleView implements ICoordinateGetter {
     	return new Point(0, 0);	
     }
 
-    void show(final FieldSmall field) {
+        void show(final FieldSmall field) {
 	    final StringBuilder fieldBuilder = new StringBuilder();
-	    for (int x = 0; x < field.getSize(); x++) {
-            if (x != 0)
-                generateSeparator(fieldBuilder);
-            generateLine(field, x, fieldBuilder);
+	        for (int x = 0; x < field.getSize(); x++) {
+                if (x != 0)
+                    generateSeparator(fieldBuilder);
+                    generateLine(field, x, fieldBuilder);
         }
 	    System.out.println(fieldBuilder.toString());
     }
 
-    void generateLine(final FieldSmall field,
-                           final int x,
-			   final StringBuilder sb) {
-        for (int y = 0; y < field.getSize(); y++) {
-            if (y != 0)
-		sb.append("|");
-	    sb.append(" ");
-            final Figure figure;
-            figure = field.getFigure(new Point(y, x));
-	    sb.append(figure != null ? figure : " ");
-	    sb.append(" ");
+        void generateLine(final FieldSmall field,
+                          final int x,
+                          final StringBuilder sb) {
+                            for (int y = 0; y < field.getSize(); y++) {
+                                if (y != 0)
+		                            sb.append("|");
+	                                sb.append(" ");
+                                    final Figure figure;
+                                    figure = field.getFigure(new Point(y, x));
+	                                sb.append(figure != null ? figure : " ");
+	                                sb.append(" ");
         }
-	sb.append("\n");
+	    sb.append("\n");
     }
 
-    void generateSeparator(final StringBuilder sb) {
+        void generateSeparator(final StringBuilder sb) {
 	sb.append("~~~~~~~~~~~\n");
     }
 
